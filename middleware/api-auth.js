ee777c4a3afa1e61974acf43766fc58df5945ea9
Tiny_Murky <murky0830@gmail.com>
2023-10-13 11:38:35 +0800

    feat: add custom errors and error-handlers custom error is in ./libs/error/custome-error.js. Already defined some common error with status, when using thos errors, you can only provide message, it has default response status. But tuou can also provide your own status. Error handle will return 500 if error is not in custom error.

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,8 +1,11 @@
 const passport = require('../config/passport')
-
+const { AuthorizationError } = require('../libs/error/custom-error')
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    if (err || !user) {
+      throw new AuthorizationError('Unauthorized')
+    }
+
     req.user = user
     next()
   })(req, res, next)
