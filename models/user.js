a066bd776a7bdc17ed0d3831df51b4d9b9f438e7
Tiny_Murky <murky0830@gmail.com>
2023-10-15 12:54:59 +0800

    feat: add cart model and associate to user

--- models/user.js
+++ models/user.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      User.hasOne(models.Cart, {
+        foreignKey: 'userId'
+      })
     }
   }
   User.init({
