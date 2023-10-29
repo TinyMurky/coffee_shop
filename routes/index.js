a5b153cd7e1c7004ae2e3ee44f761b4725d09963
Tiny_Murky <murky0830@gmail.com>
2023-10-29 21:59:13 +0800

    feat: add /email router for testing, now can email successfully

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const auth = require('./modules/auth')
 const cart = require('./modules/cart')
 const product = require('./modules/product')
 const event = require('./modules/event')
+const email = require('./modules/email')
 const { authenticated } = require('../middleware/api-auth')
 const errorHandler = require('../middleware/error-handler')
 
@@ -11,6 +12,7 @@ router.use('/auth', auth)
 router.use('/cart', authenticated, cart)
 router.use('/products', product) // 需要提供給未登入用戶看
 router.use('/events', event)
+router.use('/emails', email)
 
 router.use('/', errorHandler)
 
